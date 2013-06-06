e29a4aead0f3739eafbe9d78a34acbf2d9cee108
Robin Luckey <robinluckey@gmail.com>
2013-06-06 02:40:26 +0000

    Symbols now retired; replaced with variables
     ':' defines (or re-defines) a 16-bit symbol at the current location
     '&' emits the symbol
     '>' and '<' emits the high or low byte of the symbol

--- asm.asm
+++ asm.asm
@@ -13,12 +13,12 @@
 ;	84-85   vnext ; pointer to next available vtable entry
 
 *2000	:buf		; general-purpose string buffer
-*3000	:vtable	; variable-length symbol table
+*3000	:vtable		; variable-length symbol table
 
 *1000
 
 :main
-	4C @init	; JMP init
+	4C &init	; JMP init
 
 :incr_loc
 	E680		; INC 80
@@ -27,7 +27,7 @@
 	60		; RTS
 
 :skip_comment
-	20 @getchar	; JSR getchar
+	20 &getchar	; JSR getchar
 	C90A		; CMP #'\n'
 	D0 -07		; BNE -7
 	60		; RTS
@@ -38,7 +38,7 @@
 	C901		; CMP #1
 	D005		; BNE +5
 	68		; PLA
-	20 @putchar	; JSR putchar
+	20 &putchar	; JSR putchar
 	60		; RTS
 	68		; PLA
 	60		; RTS
@@ -55,13 +55,13 @@
 :read_var		; Reads a variable name from stdin and stores it in &A, offset
 			; by 2 bytes to match vtable records.
 			;
-	A9 [buf		; LDA #lo(buf)
+	A9 <buf		; LDA #lo(buf)
 	8502		; STA pbuf
-	A9 ]buf		; LDA #hi(buf)
+	A9 >buf		; LDA #hi(buf)
 	8503		; STA pbuf+1
 	A002		; LDY #2	; Note 2-byte offset
 :read_var_loop
-	20 @getchar	; JSR getchar
+	20 &getchar	; JSR getchar
 	C9 " "		; CMP #' '
 	F00E		; BEQ read_var_end
 	C909		; CMP #'\t'
@@ -91,13 +91,13 @@
 			;
 			; Addresses 02-03 will be used as temp pointer into buffer A.
 			;
-	A9 [vtable		; LDA #lo(vtable); begin at top of vtable
+	A9 <vtable		; LDA #lo(vtable); begin at top of vtable
 	8500		; STA pv
-	A9 ]vtable		; LDA #hi(vtable)
+	A9 >vtable		; LDA #hi(vtable)
 	8501		; STA pv+1
-	A9 [buf		; LDA #lo(buf)	; temp pointer into buffer A
+	A9 <buf		; LDA #lo(buf)	; temp pointer into buffer A
 	8502		; STA pbuf
-	A9 ]buf		; LDA #hi(buf)
+	A9 >buf		; LDA #hi(buf)
 	8503		; STA pbuf+1
 :seek_var_each
 	A500		; LDA pv	; reached end of vtable?
@@ -131,7 +131,7 @@
 	8500		; STA 00
 	9002		; BCC +2
 	E601		; INC 01
-	4C @seek_var_each ; JUMP
+	4C &seek_var_each ; JUMP
 :seek_var_found
 	A901		; LDA #01	; variable exists
 	60		; RTS
@@ -143,53 +143,53 @@
 			;
 			; Addresses 00-03 will be destroyed.
 			;
-	20 @read_var	; JSR read_var  ; sets 02-03 to variable name buffer
-	20 @incr_loc	; JSR incr_loc
-	20 @incr_loc	; JSR incr_loc
+	20 &read_var	; JSR read_var  ; sets 02-03 to variable name buffer
+	20 &incr_loc	; JSR incr_loc
+	20 &incr_loc	; JSR incr_loc
 			;
 	A582		; LDA 82	; which pass?
 	C900		; CMP #0
 	F011		; BEQ get_var_end
 			;
-	20 @seek_var	; JSR seek_var	; sets 00-01 to vtable record
+	20 &seek_var	; JSR seek_var	; sets 00-01 to vtable record
 	A000		; LDY #00	; lo value
 	B100		; LDA (pv),Y
-	20 @emit	; JSR emit
+	20 &emit	; JSR emit
 	A001		; LDY #01	; hi value
 	B100		; LDA (pv),Y
-	20 @emit	; JSR emit
+	20 &emit	; JSR emit
 :get_var_end
 	60		; RTS
 
 :get_hi			; Same as get_var, but emits hi byte only
 			;
-	20 @read_var	; JSR read_var  ; sets 02-03 to variable name buffer
-	20 @incr_loc	; JSR incr_loc
+	20 &read_var	; JSR read_var  ; sets 02-03 to variable name buffer
+	20 &incr_loc	; JSR incr_loc
 			;
 	A582		; LDA 82	; which pass?
 	C900		; CMP #0
 	F00A		; BEQ get_hi_end
 			;
-	20 @seek_var	; JSR seek_var	; sets 00-01 to vtable record
+	20 &seek_var	; JSR seek_var	; sets 00-01 to vtable record
 	A001		; LDY #01	; hi value
 	B100		; LDA (pv),Y
-	20 @emit	; JSR emit
+	20 &emit	; JSR emit
 :get_hi_end
 	60		; RTS
 
 :get_lo			; Same as get_var, but emits lo byte only
 			;
-	20 @read_var	; JSR read_var  ; sets 02-03 to variable name buffer
-	20 @incr_loc	; JSR incr_loc
+	20 &read_var	; JSR read_var  ; sets 02-03 to variable name buffer
+	20 &incr_loc	; JSR incr_loc
 			;
 	A582		; LDA 82	; which pass?
 	C900		; CMP #0
 	F00A		; BEQ get_lo_end
 			;
-	20 @seek_var	; JSR seek_var	; sets 00-01 to vtable record
+	20 &seek_var	; JSR seek_var	; sets 00-01 to vtable record
 	A000		; LDY #00	; lo value
 	B100		; LDA (pv),Y
-	20 @emit	; JSR emit
+	20 &emit	; JSR emit
 :get_lo_end
 	60		; RTS
 
@@ -201,8 +201,8 @@
 			;
 			; Addresses 00-03 will be destroyed.
 			;
-	20 @read_var	; JSR read_var  ; sets 02-03 to variable name buffer
-	20 @seek_var	; JSR seek_var	; sets 00-01 to vtable record
+	20 &read_var	; JSR read_var  ; sets 02-03 to variable name buffer
+	20 &seek_var	; JSR seek_var	; sets 00-01 to vtable record
 	48		; PHA		; 0 if record did not exist (new vtable entry)
 			;
 	A000		; LDY #00	; save location counter value
@@ -241,27 +241,27 @@
 	4A		; LSR A
 	A8		; TAY		; lookup hex char
 	18		; CLC
-	B9 @hex_digits	; LDA hex_digits,Y
-	20 @putchar	; JSR putchar
+	B9 &hex_digits	; LDA hex_digits,Y
+	20 &putchar	; JSR putchar
 	8A		; TXA		; restore
 	290F		; AND #0F	; low order nibble
 	A8		; TAY		; lookup hex char
 	18		; CLC
-	B9 @hex_digits	; LDA hex_digits,Y
-	20 @putchar	; JSR putchar
+	B9 &hex_digits	; LDA hex_digits,Y
+	20 &putchar	; JSR putchar
 	60		; RTS
 
 :print_variable_table
 	A9 "P"		; LDA #"P"
-	20 @putchar	; JSR putchar
+	20 &putchar	; JSR putchar
 	A901		; LDA #1
-	20 @printhex	; JSR printhex
+	20 &printhex	; JSR printhex
 	A90A		; LDA #"\n"
-	20 @putchar	; JSR putchar
+	20 &putchar	; JSR putchar
 			;
-	A9 [vtable	; LDA lo(vtable)
+	A9 <vtable	; LDA lo(vtable)
 	8500		; STA 00	; 00-01: temp pointer into vtable
-	A9 ]vtable	; LDA hi(vtable)
+	A9 >vtable	; LDA hi(vtable)
 	8501		; STA 01
 :pv_loop
 	A500		; LDA 00	; at end of vtable?
@@ -273,30 +273,30 @@
 	60		; RTS
 
 	A9 "*"		; LDA #"*"
-	20 @putchar	; JSR putchar
+	20 &putchar	; JSR putchar
 	A001		; LDY #01	; print address -- high byte first!
 	B100		; LDA (00),Y
-	20 @printhex		; JSR printhex
+	20 &printhex		; JSR printhex
 	A000		; LDY #00
 	B100		; LDA (00),Y
-	20 @printhex		; JSR printhex
+	20 &printhex		; JSR printhex
 	A9 " "		; LDA #" "
-	20 @putchar	; JSR putchar
+	20 &putchar	; JSR putchar
 	A9 ":"		; LDA #":"
-	20 @putchar	; JSR putchar
+	20 &putchar	; JSR putchar
 
 	A002		; LDY #02
 :pv_name_loop
 	B100		; LDA (00),Y
 	C900		; CMP #0
 	F007		; BEQ pv_end_of_name
-	20 @putchar	; JSR putchar
+	20 &putchar	; JSR putchar
 	C8		; INY
 	D0 -0C		; BNE :pv_name_loop
 	00		; BRK		; error -- variable name too long
 :pv_end_of_name
 	A90A		; LDA #"\n"
-	20 @putchar	; JSR putchar
+	20 &putchar	; JSR putchar
 	C8		; INY		; update temp pointer to next element
 	98		; TYA
 	18		; CLC
@@ -304,27 +304,27 @@
 	8500		; STA 00
 	9002		; BCC +2
 	E601		; INC 01
-	4C @pv_loop	; JMP
+	4C &pv_loop	; JMP
 
 :string_literal
-	20 @getchar	; JSR getchar
+	20 &getchar	; JSR getchar
 	C922		; CMP #'"'
 	D001		; BNE +1
 	60		; RTS
-	20 @emit	; JSR emit
-	20 @incr_loc	; JSR incr_loc
-	4C @string_literal ; JMP
+	20 &emit	; JSR emit
+	20 &incr_loc	; JSR incr_loc
+	4C &string_literal ; JMP
 
 :twos_comp
 	; Read a hex byte from stdin, then emit
 	; the negation of that byte.
-	20 @getchar	; JSR getchar
-	20 @parse_hex_byte	; JSR parse_hex_byte
+	20 &getchar	; JSR getchar
+	20 &parse_hex_byte	; JSR parse_hex_byte
 	49FF		; EOR #FF
 	18		; CLC
 	6901		; ADC #1
-	20 @emit	; JSR emit
-	20 @incr_loc	; JSR incr_loc
+	20 &emit	; JSR emit
+	20 &incr_loc	; JSR incr_loc
 	60		; RTS
 
 :parse_hex_byte
@@ -342,7 +342,7 @@
 	0A		; ASL A
 	8510		; STA 10
 					; lo nibble
-	20 @getchar	; JSR getchar
+	20 &getchar	; JSR getchar
 	C93A		; CMP #3A
 	9002		; BCC .+2
 	69F8		; ADC #F8
@@ -351,36 +351,36 @@
 	60		; RTS
 
 :set_org
-	20 @getchar	; JSR getchar
-	20 @parse_hex_byte ; JSR parse_hex_byte
+	20 &getchar	; JSR getchar
+	20 &parse_hex_byte ; JSR parse_hex_byte
 	8581		; STA 81	; MSB first
-	20 @getchar	; JSR getchar
-	20 @parse_hex_byte ; JSR parse_hex_byte
+	20 &getchar	; JSR getchar
+	20 &parse_hex_byte ; JSR parse_hex_byte
 	8580		; STA 80	; 16-bit location counter
 	60		; RTS
 
 :set_pass
-	20 @getchar	; JSR getchar
-	20 @parse_hex_byte ; JSR parse_hex_byte
+	20 &getchar	; JSR getchar
+	20 &parse_hex_byte ; JSR parse_hex_byte
 	8582		; STA 82
 	60		; RTS
 
 :init
-	A9 [vtable	; LDA lo(vtable)
+	A9 <vtable	; LDA lo(vtable)
 	8584		; STA pnext
-	A9 ]vtable	; LDA hi(vtable)
+	A9 >vtable	; LDA hi(vtable)
 	8585		; STA pnext+1
 	A900		; LDA #00	; pass 0 by default
 	8582		; STA pass
 
 :main_loop
-	20 @getchar	; JSR getchar
+	20 &getchar	; JSR getchar
 	C9FF		; CMP #FF	; EOF?
 	D00A		; BNE +10
 	A582		; LDA 82
 	C900		; CMP #0
 	D003		; BNE +3
-	20 @print_variable_table ; JSR
+	20 &print_variable_table ; JSR
 	00		; BRK
 			;
 	C9 " "		; CMP #' '	; skip white space
@@ -392,56 +392,56 @@
 			;		; switch on pseudo-op
 	C9 "*"		; CMP #'*'
 	D006		; BNE +6
-	20 @set_org	; JSR set_org
-	4C @main_loop	; JMP
+	20 &set_org	; JSR set_org
+	4C &main_loop	; JMP
 			;
-	C9 "]"		; CMP #']'
+	C9 ">"		; CMP #'>'
 	D006		; BNE +6
-	20 @get_hi	; JSR get_hi
-	4C @main_loop	; JMP
+	20 &get_hi	; JSR get_hi
+	4C &main_loop	; JMP
 			;
-	C9 "["		; CMP #'['
+	C9 "<"		; CMP #'<'
 	D006		; BNE +6
-	20 @get_lo	; JSR get_lo
-	4C @main_loop	; JMP
+	20 &get_lo	; JSR get_lo
+	4C &main_loop	; JMP
 			;
 	C922		; CMP #'"'
 	D006		; BNE +6
-	20 @string_literal	; JSR string_literal
-	4C @main_loop	; JMP
+	20 &string_literal	; JSR string_literal
+	4C &main_loop	; JMP
 			;
 	C9 ";"		; CMP #';'
 	D006		; BNE +6
-	20 @skip_comment	; JSR skip_comment
-	4C @main_loop	; JMP
+	20 &skip_comment	; JSR skip_comment
+	4C &main_loop	; JMP
 			;
 	C9 "P"		; CMP #'P'
 	D006		; BNE +6
-	20 @set_pass	; JSR set_pass
-	4C @main_loop	; JMP
+	20 &set_pass	; JSR set_pass
+	4C &main_loop	; JMP
 			;
 	C9 "-"		; CMP #'-'
 	D006		; BNE +6
-	20 @twos_comp	; JSR twos_comp
-	4C @main_loop	; JMP
+	20 &twos_comp	; JSR twos_comp
+	4C &main_loop	; JMP
 			;
 	C9 ":"		; CMP #':'
 	D006		; BNE +6
-	20 @set_var	; JSR set_var
-	4C @main_loop	; JMP
+	20 &set_var	; JSR set_var
+	4C &main_loop	; JMP
 			;
-	C9 "@"		; CMP #'@'
+	C9 "&"		; CMP #'&'
 	D006		; BNE +6
-	20 @get_var	; JSR get_var
-	4C @main_loop	; JMP
+	20 &get_var	; JSR get_var
+	4C &main_loop	; JMP
 			;
 			; no pseudo-op; emit raw byte
 			;
-	20 @parse_hex_byte ; JSR parse_hex_byte
-	20 @emit	; JSR emit
-	20 @incr_loc	; JSR incr_loc
+	20 &parse_hex_byte ; JSR parse_hex_byte
+	20 &emit	; JSR emit
+	20 &incr_loc	; JSR incr_loc
 			;
-	4C @main_loop	; JMP
+	4C &main_loop	; JMP
 
 :hex_digits
 	"0123456789ABCDEF"
