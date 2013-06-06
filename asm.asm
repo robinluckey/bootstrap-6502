1098c6dafd644413ce8c9b1694a548edc0330956
Robin Luckey <robinluckey@gmail.com>
2013-06-06 01:43:03 +0000

    Refactor vtable code
    Pulled out some common elements so that I can re-implement '<' and '>'
    for variables (not just one-letter symbols).

--- asm.asm
+++ asm.asm
@@ -10,7 +10,7 @@
 ;
 ;	80-81	location counter
 ;	82	assembly pass (0 or 1)
-;	84-85   vnext -- pointer to next available variable-length symbol table entry
+;	84-85   vnext ; pointer to next available vtable entry
 
 *2000	.A		; general-purpose string buffer
 *3000	.V		; vtable -- variable-length symbol table
@@ -69,8 +69,17 @@
 	20 @incr_loc	; JSR incr_loc
 	60		; RTS
 
-:read_var		; Reads a variable name from stdin and stores it in buffer .A.
-			; Variable name will be offset by 2 bytes to match vtable records.
+; The variable table (vtable) is a packed array of null-terminated records:
+;
+;	2 bytes: value (16-bit memory address)
+;	n bytes: name (variable length string)
+;	1 byte:  0 terminator
+;
+; The table begins at &V. The pointer vnext (84-85) always points to the first
+; empty byte beyond the end of the vtable.
+
+:read_var		; Reads a variable name from stdin and stores it in &A, offset
+			; by 2 bytes to match vtable records.
 			;
 	A9 <A		; LDA #lo(buf)
 	8502		; STA pbuf
@@ -95,47 +104,121 @@
 	60		; RTS
 
 :seek_var
-			; Finds or creates a variable name in the vtable.
-			;
+			; Finds a named variable in the vtable.
 			; Variable name to be matched must be stored in buffer A.
+			; Variable name should be offset by 2 bytes to match vtable records.
 			;
-			; Upon return, 00-01 will contain pointer into vtable.
-			; Address 02-03 will be used as temp pointer into buffer A.
+			; If the variable is found, the return accumulator will be 1,
+			; and addresses 00-01 will point at its vtable entry.
 			;
-			; If the variable is not found, it will be created. A new
-			; record will be appended to the end of the vtable.
-
-:defvar
-			; The symbol table is a packed array of records:
+			; If the variable is not found, the accumulator will be 0,
+			; and the pointer will be equal to vnext, beyond the end of the
+			; vtable. The caller may append a new value here.
 			;
-			;	2 bytes: value (memory address)
-			;	n bytes: string name
-			;	1 byte:  0 terminator
+			; Addresses 02-03 will be used as temp pointer into buffer A.
 			;
-	A000		; LDY #00
-	A580		; LDA 80	; store location counter value
-	9184		; STA (vnext),Y
+	A9 <V		; LDA #lo(vtable); begin at top of vtable
+	8500		; STA pv
+	A9 >V		; LDA #hi(vtable)
+	8501		; STA pv+1
+	A9 <A		; LDA #lo(buf)	; temp pointer into buffer A
+	8502		; STA pbuf
+	A9 >A		; LDA #hi(buf)
+	8503		; STA pbuf+1
+:seek_var_each
+	A500		; LDA pv	; reached end of vtable?
+	C584		; CMP vnext
+	D009		; BNE seek_var_cmp
+	A501		; LDA pv+1
+	C585		; CMP vnext+1
+	D003		; BNE seek_var_cmp
+	A900		; LDA #00	; variable does not exist
+	60		; RTS
+:seek_var_cmp
+	A002		; LDY #2	; variable name begins after 2-byte address
+:seek_var_cmp_loop
+	B100		; LDA (pv),Y
+	D102		; CMP (pbuf),Y
+	D008		; BNE seek_var_skip
+	C900		; CMP #0	; reached end of name -> successful match
+	F018		; BEQ seek_var_found
 	C8		; INY
-	A581		; LDA 81
-	9184		; STA (vnext),Y
-	C8		; INY
-:defvar_loop				; read and store variable name
-	20 @getchar	; JSR getchar
-	C90A		; CMP #'\n'
-	F00E		; BEQ defvar_end
-	C909		; CMP #'\t'
-	F00A		; BEQ defvar_end
-	C9 " "		; CMP #' '
-	F006		; BEQ defvar_end
-	9184		; STA (vnext),Y
-	C8		; INY
-	D0 -14		; BNE defvar_loop
+	D0 -0D		; BNE seep_var_cmp_loop
 	00		; BRK		; error -- variable name too long
-:defvar_end
-	A900		; LDA #00
-	9184		; STA (vnext),Y
+:seek_var_skip
+	B100		; LDA (pv),Y	; seek to end of unmatched name
 	C8		; INY
+	C900		; CMP #0
+	D0 -07		; BNE seek_var_skip
+:seek_var_next
+	98		; TYA		; move pv to next variable in vtable
+	18		; CLC
+	6500		; ADC 00
+	8500		; STA 00
+	9002		; BCC +2
+	E601		; INC 01
+	4C @seek_var_each ; JUMP
+:seek_var_found
+	A901		; LDA #01	; variable exists
+	60		; RTS
 
+:get_var		; Reads a variable name from stdin, then
+			;
+			;   ... during pass 0, increments location counter only.
+			;   ... during pass 1, also evaluates and emits its value.
+			;
+			; Addresses 00-03 will be destroyed.
+			;
+	20 @read_var	; JSR read_var  ; sets 02-03 to variable name buffer
+	20 @incr_loc	; JSR incr_loc
+	20 @incr_loc	; JSR incr_loc
+			;
+	A582		; LDA 82	; which pass?
+	C900		; CMP #0
+	F011		; BEQ get_var_end
+			;
+	20 @seek_var	; JSR seek_var	; sets 00-01 to vtable record
+	A000		; LDY #00	; lo value
+	B100		; LDA (pv),Y
+	20 @emit	; JSR emit
+	A001		; LDY #01	; hi value
+	B100		; LDA (pv),Y
+	20 @emit	; JSR emit
+:get_var_end
+	60		; RTS
+
+:set_var		; Reads a variable name from stdin, then writes an entry
+			; in the vtable using the current location counter as its value.
+			;
+			; An existing variable will be overwritten. New variables will
+			; be appended to the end of the vtable.
+			;
+			; Addresses 00-03 will be destroyed.
+			;
+	20 @read_var	; JSR read_var  ; sets 02-03 to variable name buffer
+	20 @seek_var	; JSR seek_var	; sets 00-01 to vtable record
+	48		; PHA		; 0 if record did not exist (new vtable entry)
+			;
+	A000		; LDY #00	; save location counter value
+	A580		; LDA loc
+	9100		; STA (pv),Y
+	C8		; INY
+	A581		; LDA loc+1
+	9100		; STA (pv),Y
+:set_var_loop
+	C8		; INY		; save variable name
+	B102		; LDA (pbuf),Y
+	9100		; STA (pv),Y
+	C900		; CMP #0
+	D0 -09		; BNE set_var_loop
+			;
+			; If we have extended the vtable, we must update the end pointer.
+			;
+	68		; PLA
+	C900		; CMP #00
+	F001		; BEQ +01
+	60		; RTS
+	C8		; INY
 	98		; TYA		; update vnext
 	18		; CLC
 	6584		; ADC 84
@@ -243,64 +326,6 @@
 	E601		; INC 01
 	4C @pv_loop	; JMP
 
-:getvar
-	; reads variable name from stdin, emits its value to stdout
-	;
-	; 00-01: (pv)   iteration pointer into vtable
-	; 02-03: (pbuf) iteration pointer into input buffer
-	20 @read_var	; JSR read_var
-:getvar_search				; find matching name in table
-	A582		; LDA 82	; if it's pass 0, just accept input and ignore
-	C900		; CMP #0
-	D007		; BNE +7
-	20 @incr_loc	; JSR incr_loc
-	20 @incr_loc	; JSR incr_loc
-	60		; RTS
-
-	A9 <V		; LDA #lo(vtable); begin at top of vtable
-	8500		; STA pv
-	A9 >V		; LDA #hi(vtable)
-	8501		; STA pv+1
-:getvar_compare
-	A002		; LDY #2	; variable name begins after 2-byte address
-	B100		; LDA (pv),Y
-	D102		; CMP (pbuf),Y
-	D008		; BNE getvar_skip
-	C900		; CMP #0	; end of name -> successful match
-	F022		; BEQ getvar_found
-	C8		; INY
-	D0 -0D		; BNE
-	00		; BRK		; error -- variable name too long
-:getvar_skip
-	B100		; LDA (pv),Y	; find end of variable in table
-	C8		; INY
-	C900		; CMP #0
-	D0 -07		; BNE getvar_skip
-:getvar_next				; move pv to next variable in vtable
-	98		; TYA
-	18		; CLC
-	6500		; ADC 00
-	8500		; STA 00
-	9002		; BCC +2
-	E601		; INC 01
-	A500		; LDA 00	; at end of vtable?
-	C584		; CMP vnext
-	D0 -27		; BNE getvar_compare
-	A501		; LDA 01
-	C585		; CMP nvext+1
-	D0 -2D		; BNE getvar_compare
-	00		; BRK		; error -- variable not found
-:getvar_found
-	A000		; LDY #00	; vtable record lo address
-	B100		; LDA (pv),Y
-	20 @emit	; JSR emit
-	20 @incr_loc	; JSR incr_loc
-	A001		; LDY #01	; vtable record hi address
-	B100		; LDA (pv),Y
-	20 @emit	; JSR emit
-	20 @incr_loc	; JSR incr_loc
-	60		; RTS
-
 :print_symbol_table
 	A9 "P"		; LDA #"P"
 	20 @putchar	; JSR putchar
@@ -400,11 +425,11 @@
 
 :init
 	A9 <V		; LDA lo(vtable)
-	8584		; STA 84
+	8584		; STA pnext
 	A9 >V		; LDA hi(vtable)
-	8585		; STA 85
-	A900		; LDA #00
-	8582		; STA 82	; pass 0 by default
+	8585		; STA pnext+1
+	A900		; LDA #00	; pass 0 by default
+	8582		; STA pass
 
 :main_loop
 	20 @getchar	; JSR getchar
@@ -471,12 +496,12 @@
 			;
 	C9 ":"		; CMP #':'
 	D006		; BNE +6
-	20 @defvar	; JSR defvar
+	20 @set_var	; JSR set_var
 	4C @main_loop	; JMP
 			;
 	C9 "@"		; CMP #'@'
 	D006		; BNE +6
-	20 @getvar	; JSR getvar
+	20 @get_var	; JSR get_var
 	4C @main_loop	; JMP
 			;
 			; no pseudo-op; emit raw byte
